946e8c3979594b68279ad4b9954ae9a4671b21c8
Felipe <felipe.desouza1992@gmail.com>
2020-06-17 12:21:31 -0300

    firt commit

---
+++ src/trabalho/Professor.java
@@ -0,0 +1,30 @@
+package trabalho;
+
+public class Professor extends Pessoa {
+
+
+    private double salario;
+    private String nomeCurso ;
+
+
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public String getNomeCurso() {
+        return nomeCurso;
+    }
+
+    public void setNomeCurso(String nomeCurso) {
+        this.nomeCurso = nomeCurso;
+    }
+    public double calcularSalarioliquido(){
+        return 0;
+
+    }
+}
---
+++ src/trabalho/teste.java
@@ -0,0 +1,25 @@
+package trabalho;
+
+public class teste {
+
+    public static void main(String[] args) {
+
+       /* Aluno aluno = new Aluno();
+
+        Professor professor = new Professor();
+
+        Pessoa pessoa = new Pessoa();
+
+        aluno.setNome("Maria");
+*/
+       // professor.setTelefoneCelular(764327642734723);
+
+
+        Pessoa aluno = new Aluno();
+        //aluno.get
+
+Pessoa professor = new Professor()
+        professor.get
+    }
+
+}
